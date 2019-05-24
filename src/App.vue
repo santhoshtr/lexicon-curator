98d10a5ac55623077a83943b8c8267f1919402d6
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-05-24 21:56:31 +0530

    Avoid dialogs and use inline editing

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <span>Lexicon Curator</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-       <v-btn flat href="https://gitlab.com/smc/mlmorph" target="_blank">
+      <v-btn flat href="https://gitlab.com/smc/mlmorph" target="_blank">
         <span class="mr-2">Mlmorph</span>
       </v-btn>
       <v-dialog v-model="dialog" max-width="500px">
@@ -27,7 +27,6 @@
                 </v-flex>
                 <v-flex xs12 sm6 md4>
                   <v-select
-                    solo
                     v-model="newItem.pos"
                     :items="pos"
                     label="Part of speech"
@@ -46,70 +45,32 @@
           </v-card-actions>
         </v-card>
       </v-dialog>
-
     </v-toolbar>
 
     <v-content>
       <v-data-table :headers="headers" :items="lexicon" class="elevation-1">
         <template v-slot:items="props">
-          <td>
-            <v-edit-dialog
-              :return-value.sync="props.item.word"
-              lazy
-              persistent
-              autofocus
-              large
-              @save="save(props.item)"
-              @cancel="cancel"
-              @open="open"
-              @close="close"
-            >
-              {{ props.item.word }}
-              <template v-slot:input>
-                <v-text-field
-                  v-model="props.item.word"
-                  :rules="[noSpace, noEnglish]"
-                  label="Edit"
-                  single-line
-                  counter
-                ></v-text-field>
-              </template>
-            </v-edit-dialog>
+          <td class="text-xs-right word">
+            <v-text-field  v-model="props.item.word" :rules="[noSpace, noEnglish]"></v-text-field>
           </td>
-          <td class="text-xs-right">
-            <v-edit-dialog
-              :return-value.sync="props.item.pos||true"
-              lazy
-              persistent
-              large
-              @save="save(props.item)"
-              @cancel="cancel"
-              @open="open"
-              @close="close"
-            >
-              {{ posLabel(props.item.pos) || "Select" }}
-              <template v-slot:input>
-                <v-select
-                  solo
-                  v-model="props.item.pos"
-                  :items="pos"
-                  label="Part of speech"
-                  item-text="tag"
-                  item-value="id"
-                ></v-select>
-              </template>
-            </v-edit-dialog>
+          <td class="text-xs-right pos">
+            <v-select
+              v-model="props.item.pos"
+              :items="pos"
+              item-text="tag"
+              item-value="id"
+              placeholder="Select part of speech"
+            ></v-select>
           </td>
 
-
-          <td class="justify-center layout px-0" v-if="props.item.pos">
+          <td class="justify-center layout px-0 actions" v-if="props.item.pos">
             <v-icon
               v-if="props.item.pos && props.item.reviewed"
               color="primary"
               @click="toggle_done(props.item)"
             >check_circle</v-icon>
             <v-icon v-else color="secondary" @click="toggle_done(props.item)">check</v-icon>
-              <v-icon color="secondary" @click="remove(props.item)">clear</v-icon>
+            <v-icon color="secondary" @click="remove(props.item)">clear</v-icon>
           </td>
           <td v-else class="justify-center layout px-0">
             <v-icon color="secondary" disabled>check</v-icon>
@@ -154,9 +115,9 @@ export default {
     noSpace: v => v.indexOf(" ") < 0 || "Space not allowed",
     noEnglish: v => v.match(/[a-zA-Z0-9]/) === null || "Please enter Malayalam",
     headers: [
-      { text: "Word", value: "word", align: "left" },
-      { text: "Part of speech", value: "pos", align: "left" },
-      { text: "Actions", value: "name", sortable: true, align: "left" }
+      { text: "Word", value: "word", align: "left", width: "50%" },
+      { text: "Part of speech", value: "pos", align: "left", width: "40%" },
+      { text: "Actions", value: "name", sortable: true, align: "left" , width: "10%"}
     ],
     newItem: {
       word: "",
@@ -194,7 +155,7 @@ export default {
       lexiconRef.push(newItem);
     },
     remove(item) {
-       lexiconRef.child(item[".key"]).remove()
+      lexiconRef.child(item[".key"]).remove();
     },
     close() {
       this.dialog = false;
@@ -208,3 +169,22 @@ export default {
   }
 };
 </script>
+<style>
+input {
+  border: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 'Manjari', sans-serif;
+  font-size: 1em;
+}
+.elevation-1 table.v-table thead td:not(:nth-child(1)),
+.elevation-1 table.v-table tbody td:not(:nth-child(1)),
+.elevation-1 table.v-table tbody td:first-child,
+.elevation-1 table.v-table thead th:first-child,
+.elevation-1 table.v-table tbody th:first-child {
+  padding: 10px 2px 10px 2px;
+}
+
+.word .v-text-field input[type="text"] {
+  min-width: 12ch;
+}
+
+</style>
